df79aa332ab1c478c7b34f2fa58247fb9d4dfced
m13ha <nwaokocham@gmail.com>
2022-06-12 02:14:22 +0100

    added persistent drawer for mobile nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Container from "@mui/material/Container";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import React, { useEffect, useState } from "react";
-import { PaletteMode } from "@mui/material";
-//import CircularProgress from "@mui/material/CircularProgress";
-//import Box from "@mui/material/Box";
-//import Stack from "@mui/material/Stack";
+import { PaletteMode, Typography } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
 
 function App() {
-
-  const [theme, setTheme] = React.useState<PaletteMode>("dark")
+  const [theme, setTheme] = React.useState<PaletteMode>("dark");
 
   const currentTheme = createTheme({
     palette: {
@@ -19,26 +19,69 @@ function App() {
   });
 
   const changeTheme = () => {
-    if(currentTheme.palette.mode === "light"){
+    if (currentTheme.palette.mode === "light") {
       setTheme("dark");
-    }else {
+    } else {
       setTheme("light");
     }
-    
-  }
+  };
+
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  }));
 
   return (
-    <ThemeProvider theme={currentTheme}>
-      <div className="App">
-        <Container
-          sx={{ display: "flex" }}
-          maxWidth={false}
-          disableGutters={true}
-        >
-          <Navbar changeTheme={changeTheme}></Navbar>
-        </Container>
-      </div>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={currentTheme}>
+        <CssBaseline enableColorScheme />
+        <div className="App">
+          <Container
+            sx={{
+              display: "flex",
+              flexWrap: "nowrap",
+              overflowY: "hidden",
+            }}
+            maxWidth={false}
+            disableGutters={true}
+            color="default"
+          >
+            <Box
+              position="absolute"
+              component="div"
+              color="default"
+              sx={{
+                display: "flex",
+              }}
+            >
+              <Navbar changeTheme={changeTheme}></Navbar>
+            </Box>
+            <Box
+              component="div"
+              color="default"
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                height: "100%",
+                overflowY: "scroll",
+                p: 1,
+              }}
+            >
+              <DrawerHeader></DrawerHeader>
+              <Typography variant="h3" color="initial">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel rem molestiae expedita autem consectetur,
+                voluptatibus corrupti accusamus eveniet earum nesciunt quisquam officia ullam suscipit enim perferendis
+                maiores, eos molestias optio?
+              </Typography>
+            </Box>
+          </Container>
+        </div>
+      </ThemeProvider>
+    </Router>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,16 +9,32 @@ import List from "@mui/material/List";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowsLeftRightToLine, faCircleNodes, faLayerGroup, faNetworkWired, faPeopleArrowsLeftRight, faTableList } from '@fortawesome/free-solid-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import React, { useState, useMemo } from "react";
+import {
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+} from "react-router-dom";
 
 interface Props {
   changeTheme: () => void;
+}
+
+interface ListItemLinkProps {
+  icon?: React.ReactElement;
+  primary: string;
+  to: string;
+}
+
+interface Props {
+  img?: IconProp;
 }
 
 const Navbar = (props: Props) => {
@@ -42,20 +58,78 @@ const Navbar = (props: Props) => {
     justifyContent: "flex-end",
   }));
 
+
+  function ListItemLink(props: ListItemLinkProps) {
+    const { icon, primary, to } = props;
+
+    const renderLink = useMemo(
+      () =>
+        React.forwardRef<HTMLAnchorElement, Omit<RouterLinkProps, 'to'>>(function Link(
+          itemProps,
+          ref,
+        ) {
+          return <RouterLink to={to} ref={ref} {...itemProps} role={undefined} />;
+        }),
+      [to],
+    );
+
+    return (
+      <ListItem
+        sx={{
+          width: 250,
+          height: 100,
+          display: "flex",
+          alignItems: "center",
+          alignContent: "center",
+          justifyContent: "center",
+          justifyItems: "center"
+        }}
+        button
+        divider={true}
+        component={renderLink}
+      >
+        {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        <ListItemText primary={primary}
+          primaryTypographyProps={{
+            fontSize: 20,
+            fontWeight: 600,
+          }} />
+      </ListItem>
+    );
+  }
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="default">
+    <Box component="span">
+      <AppBar position="fixed" color="default" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={handleDrawerOpen}
-          >
-            <MenuIcon fontSize="inherit" />
-          </IconButton>
+          {!open &&
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'inline-flex', sm: 'inline-flex', md: 'none' }
+              }}
+              onClick={handleDrawerOpen}
+            >
+              <MenuIcon fontSize="inherit" />
+            </IconButton>}
+          {open &&
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'inline-flex', sm: 'inline-flex', md: 'none' }
+              }}
+            onClick={handleDrawerClose}>
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Dsplayground
           </Typography>
@@ -77,24 +151,66 @@ const Navbar = (props: Props) => {
       </AppBar>
       <Drawer
         sx={{
-          width: 240,
+          width: 250,
           flexShrink: 0,
           "& .MuiDrawer-paper": {
-            width: 240,
+            width: 250,
             boxSizing: "border-box",
           },
+          display: { xs: 'block', sm: 'block', md: 'none' },
         }}
         variant="persistent"
         anchor="left"
         open={open}
+        color="primary"
+      >
+        <List
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            alignContent: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          <ListItemLink to="/inbox" primary="Stacks" icon={<FontAwesomeIcon icon={faLayerGroup as IconProp} size="2x" />} />
+          <ListItemLink to="/drafts" primary="Queues" icon={<FontAwesomeIcon icon={faPeopleArrowsLeftRight as IconProp} size="2x" />} />
+          <ListItemLink to="/inbox" primary="Linked List" icon={<FontAwesomeIcon icon={faArrowsLeftRightToLine as IconProp} size="2x" />} />
+          <ListItemLink to="/drafts" primary="Trees" icon={<FontAwesomeIcon icon={faNetworkWired as IconProp} size="2x" />} />
+          <ListItemLink to="/drafts" primary="Graphs" icon={<FontAwesomeIcon icon={faCircleNodes as IconProp} size="2x" />} />
+          <ListItemLink to="/drafts" primary="Hashtables" icon={<FontAwesomeIcon icon={faTableList as IconProp} size="2x" />} />
+        </List>
+      </Drawer>
+      <Drawer
+        sx={{
+          width: 250,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: 250, boxSizing: 'border-box' },
+          display: { xs: 'none', sm: 'none', md: 'block' },
+        }}
+        variant="permanent"
+        anchor="left"
+        color="primary"
       >
         <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            <CloseIcon fontSize="inherit"/>
-          </IconButton>
         </DrawerHeader>
-        <List></List>
-        <List></List>
+        <List
+          sx={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            alignContent: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          <ListItemLink to="/inbox" primary="Stacks" icon={<FontAwesomeIcon icon={faLayerGroup as IconProp} size="2x" />} />
+          <ListItemLink to="/drafts" primary="Queues" icon={<FontAwesomeIcon icon={faPeopleArrowsLeftRight as IconProp} size="2x" />} />
+          <ListItemLink to="/inbox" primary="Linked List" icon={<FontAwesomeIcon icon={faArrowsLeftRightToLine as IconProp} size="2x" />} />
+          <ListItemLink to="/drafts" primary="Trees" icon={<FontAwesomeIcon icon={faNetworkWired as IconProp} size="2x" />} />
+          <ListItemLink to="/drafts" primary="Graphs" icon={<FontAwesomeIcon icon={faCircleNodes as IconProp} size="2x" />} />
+          <ListItemLink to="/drafts" primary="Hashtables" icon={<FontAwesomeIcon icon={faTableList as IconProp} size="2x" />} />
+        </List>
       </Drawer>
     </Box>
   );
